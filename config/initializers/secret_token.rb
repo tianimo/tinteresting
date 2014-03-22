780cfe0821b09d170a30068cb1f7e5bdd477ed18
xian wong <xian.j.wong@gmail.com>
2014-03-22 12:19:54 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tinteresting::Application.config.secret_key_base = '480bc4b4c94abf0f3aa367a3ec988410a4cb88292f51fbe30f18c2a95e59f33e4c4581e4cb2901918d64d9f1eed046d48ff875036395828566d3368add2a2128'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tinteresting::Application.config.session_store :cookie_store, key: '_tinteresting_session'
